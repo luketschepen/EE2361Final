bdf30a4138274507ee626d773d7c8a2ef085081b
Nick42-beep <69941259+Nick42-beep@users.noreply.github.com>
2024-04-24 12:33:30 -0500

    added docstring to I2C.c

--- I2C.c
+++ I2C.c
@@ -1,33 +1,40 @@
 #include "I2C.h"
 
-
+/*
+function that initializes the I2C parameters. 
+*/
 void init_I2C1(void){
     I2C1BRG = 37; //set to a clock frequency of 400 kHz section 16.3 PIC24 D.S.
     IFS1bits.MI2C1IF = 0; //Clear Interrupt Flag 
     I2C1CONbits.I2CEN = 1; //Enable I2C Mode
 }
 
+//function that initiates the start condition for I2C1 communication.
 void I2C1_start() {
     I2C1CONbits.SEN = 1;   // Initiate start condition
     while (I2C1CONbits.SEN);
 };
 
+//function that initiates the stop condition for I2C1 communication.
 void I2C1_stop() {
     I2C1CONbits.PEN = 1;   // Initiate stop condition
     while (I2C1CONbits.PEN);
 }
-// Function to write data over I2C
+
+// Function to write data over I2C1
+//data - given data to be written
 void I2C1_write(uint8_t data) {
     I2C1TRN = data;        //  I2C transimit reg
     while (I2C1STATbits.TRSTAT); // Wait for transmission to complete
 }
 
-//MAX30102 I2C now
+//function that initiates the start condition for I2C communication.
 void I2C_start() {
     I2C2CONbits.SEN = 1;   // Initiate start condition
     while (I2C2CONbits.SEN);
 };
 
+//function that initiates the stop condition for I2C communication.
 void I2C_stop() {
     I2C2CONbits.PEN = 1;   // Initiate stop condition
     while (I2C2CONbits.PEN);
@@ -39,6 +46,7 @@ void I2C_write(uint8_t data) {
 }
 
 // Function to read data over I2C
+//enables receiving mode and returns received element
 uint32_t I2C_read() {
     I2C2RCV = 0;
     I2C2CONbits.RCEN = 1; // Enable receive mode
@@ -46,24 +54,40 @@ uint32_t I2C_read() {
     return I2C2RCV;        // I2C receive re
 }
 
+/*
+ * function that initiates the repeated start condition and 
+ * waits for the start condition to complete. 
+ */
 void I2C_repeated_start() {
     I2C2CONbits.RSEN = 1;  // Initiate repeated start condition
     while (I2C2CONbits.RSEN == 1);
     //while (I2C2CONbits.RSEN); // Wait for repeated start condition to complete
 }
 
+/*
+ * function that sends acknowledgment over I2C communication.
+ */
 void I2C_ACK() {
     I2C2CONbits.ACKDT = 0;
     I2C2CONbits.ACKEN = 1;
     while (I2C2CONbits.ACKEN == 1);
 }
 
+/*
+ * function that sends non acknowledgment over I2C communication.
+ */
 void I2C_NACK() {
     I2C2CONbits.ACKDT = 1;
     I2C2CONbits.ACKEN = 1;
     while (I2C2CONbits.ACKEN == 1);
 }
 
+/*
+ * Reads a single byte of data from a specific register address over the I2C. 
+ * uses the same process as reading the PartID
+ * 
+ * reg - register address to read from 
+ */
 uint8_t I2C_read_with_params(uint8_t reg) {
     I2C_start();
 
@@ -93,7 +117,14 @@ uint8_t I2C_read_with_params(uint8_t reg) {
     return data;
 }
 
-
+/*
+ * writes a single byte of data from a specific register address over the I2C. 
+ * writes to the register with the value to put in
+ * 
+ * reg - address to read to 
+ * 
+ * val - value to put into address to match the configurations
+ */
 void I2C_write_with_params(uint8_t reg, uint8_t val) {
     I2C_start();  
     I2C_write(MAX30102_ADDRESS_WRITE); // Send device address + write mode
